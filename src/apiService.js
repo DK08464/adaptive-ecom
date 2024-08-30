301f91d6845f9a951b927182698b16d24ec2bb22
DHYEY K <dktg2022@gmail.com>
2024-08-30 10:21:52 +0530

    @rakathon

--- src/apiService.js
+++ src/apiService.js
@@ -12,3 +12,18 @@ export const fetchData = async () => {
     throw error;
   }
 };
+
+// // Example fetch request in apiService.js
+// fetch('/api/endpoint')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.error('Error fetching data:', error);
+//   });
